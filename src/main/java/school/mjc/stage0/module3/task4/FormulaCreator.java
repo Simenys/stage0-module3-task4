7c6831c62c13836128427f716a4bab7d22d66d5e
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 20:39:17 +0300

    Task solved

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a;
+        byte b;
+        short answer;
+        answer = (9*(a*a) + (5*b) + 2 + a - 7)*((a+b+4*(a*b))/2);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char one = 99;
+        char two = 104;
+        char three = 97;
+        char four = 114;
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
     }
 }
