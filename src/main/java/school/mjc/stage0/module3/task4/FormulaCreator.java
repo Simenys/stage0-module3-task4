481290036b67ffb6d8e1c63808408b8a7b57192f
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 21:42:45 +0300

    Another try

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -4,7 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
         byte a = 1;
         byte b = 2;
-        int answer = ((((9 * (a * a)) - (5 * b)) + 2 + a) - 7) * (((a + b) - ((a * b) * 4)) / 2);
+        int answer = ((((9 * (a * a)) - (5 * b)) + 2 + a) - 7) * (((a + b) - (4 * a * b)) / 2);
         System.out.println(answer);
     }
 }
