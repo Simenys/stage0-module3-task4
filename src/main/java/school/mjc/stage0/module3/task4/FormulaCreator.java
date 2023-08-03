c5ab44be50c42af6e457e37d3abeca42e7f915dc
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 20:51:23 +0300

    Formula updated again

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -5,6 +5,6 @@ public class FormulaCreator {
         byte a = 1;
         byte b = 2;
         int answer;
-        answer = (9*(a*a) - (5*b) + 2 + a - 7)*((a+b-4*(a*b))/2);
+        answer = (9*(a*a) - (5*b) + 2 + a - 7)*((a+b-(a*b*4))/2);
     }
 }
