11e5ceb2e8a2a249783946f91e519bdfded4d4f9
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-03 21:14:57 +0300

    Do not understand what does the task needs to be done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -5,7 +5,7 @@ public class FormulaCreator {
         byte a = 1;
         byte b = 2;
         int answer;
-        answer = (a*a*9 - 5*b + 2 + a - 7)*((a+b-(a*b*4))/2);
+        answer = (9*(a*a) - 5*b + 2 + a - 7)*((a+b-a*b*4)/2);
         System.out.println(answer);
     }
 }
